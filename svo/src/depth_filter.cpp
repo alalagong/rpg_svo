fddd8ae478b77bbbe440f940bc5195341b03aa82
gong <gongyiqun51237@163.com>
2019-03-02 13:16:04 +0800

    commented by gong

--- svo/src/depth_filter.cpp
+++ svo/src/depth_filter.cpp
@@ -363,21 +363,29 @@ void DepthFilter::getSeedsCopy(const FramePtr& frame, std::list<Seed>& seeds)
 //* 论文: Video-based, Real-Time Multi View Stereo 
 void DepthFilter::updateSeed(const float x, const float tau2, Seed* seed)
 {
+
   float norm_scale = sqrt(seed->sigma2 + tau2);
   if(std::isnan(norm_scale))
     return;
+  //! N (mu, sigma^2 + tau^2)  
   boost::math::normal_distribution<float> nd(seed->mu, norm_scale);
+  //! 1/s^2=1/sigma^2 + 1/tau^2
   float s2 = 1./(1./seed->sigma2 + 1./tau2);
+  //! s2 * (mu/sigma^2 + x/tau^2)
   float m = s2*(seed->mu/seed->sigma2 + x/tau2);
+  //! a/(a+b)*N ( x | mu, sigma^2 + tau^2)  
   float C1 = seed->a/(seed->a+seed->b) * boost::math::pdf(nd, x);
+  //! b/(a+b)*U(x)
   float C2 = seed->b/(seed->a+seed->b) * 1./seed->z_range;
+  //! C=C1+C2 , 归一化  
   float normalization_constant = C1 + C2;
   C1 /= normalization_constant;
   C2 /= normalization_constant;
+
   float f = C1*(seed->a+1.)/(seed->a+seed->b+1.) + C2*seed->a/(seed->a+seed->b+1.);
   float e = C1*(seed->a+1.)*(seed->a+2.)/((seed->a+seed->b+1.)*(seed->a+seed->b+2.))
           + C2*seed->a*(seed->a+1.0f)/((seed->a+seed->b+1.0f)*(seed->a+seed->b+2.0f));
-
+  //
   // update parameters
   float mu_new = C1*m+C2*seed->mu;
   seed->sigma2 = C1*(s2 + m*m) + C2*(seed->sigma2 + seed->mu*seed->mu) - mu_new*mu_new;
